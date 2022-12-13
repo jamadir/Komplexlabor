3e8e04953e04e460a09f226285a12f36a03785c5
Jannes Dirks <34141368+jamadir@users.noreply.github.com>
2022-12-13 10:28:01 +0100

    Update Steuerungsprogramm_Arduino.ino

--- Programm/Steuerungsprogramm_Arduino/Steuerungsprogramm_Arduino.ino
+++ Programm/Steuerungsprogramm_Arduino/Steuerungsprogramm_Arduino.ino
@@ -16,22 +16,27 @@ int pos2 = 0;
 
 //SETTINGS
 float filterfaktor = 0.1;
-int gyroswitch = 2;
+float gyroswitch = 0.1;
 int xoffset = 0;
 int yoffset = 0;
+float gyrodriftx;
+float gyrodrifty;
 
 bool gyroswitchflag = false;
 
 float xvals[3];
 float yvals[3];
-float diffacc = 0;
+float diffaccx = 0;
+float diffaccy = 0;
+float diffgyrox = 0;
+float diffgyroy = 0;
 
 double cycletime;
 
 
 void setup(void) {
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   myservo.attach(9);
   myservo2.attach(6);
@@ -43,13 +48,12 @@ void setup(void) {
     }
   }
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 
 }
 
 void loop() {
-  cycletime = millis();
 
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
@@ -59,27 +63,53 @@ void loop() {
     yvals[1] = accy_filtered(a.acceleration.y);
   }
 
-  if (((xvals[1] * yvals[1]) - (diffacc * diffacc)) > gyroswitch ^ 2) {
-    gyroswitchflag = true;
 
-    xvals[1] += (g.gyro.x) / ((millis() - cycletime) / 1000);
-    yvals[1] += (g.gyro.y) / ((millis() - cycletime) / 1000);
+  if (((abs(a.acceleration.x) - diffaccx) == 0) && ((abs(a.acceleration.y) - diffaccy) == 0)) {
+    //Serial.print(" Driftmessung\t");
+    //Serial.print((abs(a.acceleration.x) - diffaccx));
+    gyrodriftx = (1 - 0.05) * gyrodriftx + g.gyro.x * 0.05 * 57;
+    gyrodrifty = (1 - 0.05) * gyrodrifty + g.gyro.y * 0.05 * 57;
+
+  }
+
+  if (((abs(g.gyro.x) - diffgyrox) >= gyroswitch) || ((abs(g.gyro.y) - diffgyroy) >= gyroswitch)) {
+    gyroswitchflag = true;
+    Serial.print("GYRO: ");
+    Serial.print(micros() - cycletime);
+    Serial.print("\t");
+    Serial.print(g.gyro.x * 57 - gyrodriftx);
+    Serial.print("\t");
+    Serial.print(g.gyro.y * 57 - gyrodrifty);
+    Serial.print(((micros() - cycletime) / 1e6));
+    Serial.print("\t");
+
+    xvals[1] += map(((g.gyro.x * 57 - gyrodriftx) * ((micros() - cycletime) / 1e6)),-180,180,-10,10);
+    yvals[1] += map(((g.gyro.y * 57 - gyrodrifty) * ((micros() - cycletime) / 1e6)),-180,180,-10,10);
 
   } else {
     gyroswitchflag = false;
+    Serial.println("Accmode");
   }
+  cycletime = micros();
 
-  setservos(xvals[1] + xoffset, yvals[1] + yoffset);
 
-  diffacc = xvals[1] * yvals[1];
+  setservos(xvals[1], yvals[1]);
+
+  diffaccx = abs(a.acceleration.x);
+  diffaccy = abs(a.acceleration.y);
+  diffgyrox = abs(g.gyro.x);
+  diffgyroy = abs(g.gyro.y);
 }
 
 
-void setservos(int accx, int accy) {
-  pos = map((accx) * 1000, -10000, 10000, 1000, 2000);
-  myservo. writeMicroseconds(pos);
-  pos2 = map((accy) * 1000, -10000, 10000, 1000, 2000);
+void setservos(float accx, float accy) {
+  pos = map((accx) * 1000, -8000, 8000, 1100, 1900);
+  myservo. writeMicroseconds(pos-120);
+  Serial.print(pos);
+  Serial.print(" : ");
+  pos2 = map((accy) * 1000, -8000, 8000, 1100, 1900);
   myservo2.write(pos2);
+  Serial.println(pos2);
 }
 
 float accx_filtered(float accx) {
