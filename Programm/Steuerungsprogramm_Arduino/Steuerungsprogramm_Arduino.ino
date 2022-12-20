69b006a1cad194a27dbae9f0415563376d034ae5
Jannes Dirks <34141368+jamadir@users.noreply.github.com>
2022-12-20 06:02:19 +0100

    Entwurf Programm

--- Programm/Steuerungsprogramm_Arduino/Steuerungsprogramm_Arduino.ino
+++ Programm/Steuerungsprogramm_Arduino/Steuerungsprogramm_Arduino.ino
@@ -1,4 +1,5 @@
-// Basic demo for accelerometer readings from Adafruit MPU6050
+//Einbinden der Bibliotheken
+
 
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
@@ -10,14 +11,15 @@ Adafruit_MPU6050 mpu;
 
 Servo myservo;
 Servo myservo2;
+//Definieren Servos
 
 int pos = 0;
 int pos2 = 0;
 
-//SETTINGS
+//SETTINGS  Diese Variablen sind für den Betrieb einzustellen
 float filterfaktor = 0.1;
 float gyroswitch = 0.1;
-int xoffset = 0;
+int xoffset = -120;
 int yoffset = 0;
 float gyrodriftx;
 float gyrodrifty;
@@ -41,15 +43,17 @@ void setup(void) {
   myservo.attach(9);
   myservo2.attach(6);
 
+//Servos werden eingerichtet, der MPU6050 Chip verbunden
+
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
       delay(10);
     }
   }
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);     //Erdbeschleunigung
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);        //250°/s beschleunigung
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);      //zusätzlicher Filter
 
 }
 
@@ -58,11 +62,15 @@ void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
+  //erfassung eines Sensor-events und abspeicherung in den Variablen
+
   if (!gyroswitchflag) {
     xvals[1] = accx_filtered(a.acceleration.x);
     yvals[1] = accy_filtered(a.acceleration.y);
   }
 
+
+//dient dazu bei stillstand den Gyro Drift zu erfassen und zu filtern
 
   if (((abs(a.acceleration.x) - diffaccx) == 0) && ((abs(a.acceleration.y) - diffaccy) == 0)) {
     gyrodriftx = (1 - 0.05) * gyrodriftx + g.gyro.x * 0.05 * 57;
@@ -70,6 +78,9 @@ void loop() {
 
   }
 
+
+//Wenn eine schnelle Bewegung wahrgenommen wird, wird auf die Werte des Gyros zurückgegriffen (Genauer)
+  
   if (((abs(g.gyro.x) - diffgyrox) >= gyroswitch) || ((abs(g.gyro.y) - diffgyroy) >= gyroswitch)) {
     gyroswitchflag = true;
 
@@ -77,12 +88,12 @@ void loop() {
     yvals[1] += map(((g.gyro.y * 57 - gyrodrifty) * ((micros() - cycletime) / 1e6)),-180,180,-10,10);
 
   } else {
-    gyroswitchflag = false;
+    gyroswitchflag = false;   //Zurücksetzten auf Accellerometerbetrieb
   }
   cycletime = micros();
 
 
-  setservos(xvals[1], yvals[1]);
+  setservos(xvals[1], yvals[1]);   //setzten der Servos
 
   diffaccx = abs(a.acceleration.x);
   diffaccy = abs(a.acceleration.y);
@@ -93,10 +104,12 @@ void loop() {
 
 void setservos(float accx, float accy) {
   pos = map((accx) * 1000, -8000, 8000, 1100, 1900);
-  myservo. writeMicroseconds(pos-120);
+  myservo. writeMicroseconds(pos + xoffset);
   pos2 = map((accy) * 1000, -8000, 8000, 1100, 1900);
-  myservo2.writeMicroseconds(pos2);
+  myservo2.writeMicroseconds(pos2 + yoffset);
 }
+
+//Filterfunktionen für die Servoausgänge
 
 float accx_filtered(float accx) {
   xvals[0] = (1 - filterfaktor) * xvals[0] + accx * filterfaktor;
